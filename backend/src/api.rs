8f3309ccc17eb2e5e9aa4ab30a11d1b02667f5a3
Dennis Collective <dennis.collective@gmail.com>
2020-04-30 01:10:32 -0700

    endpoint to build activity

--- backend/src/api.rs
+++ backend/src/api.rs
@@ -1,21 +1,37 @@
-use crate::db::fetch_activities;
+use crate::db::{create_activity, fetch_activities};
 use http_types::headers::HeaderValue;
+use http_types::StatusCode;
+use serde::Serialize;
 use sqlx::PgPool;
 use tide::security::{CorsMiddleware, Origin};
-use tide::{Request, Server};
+use tide::{Request, Response, Server};
+
+type State = PgPool;
 
 pub fn new(pool: PgPool) -> Server<PgPool> {
-    let rules = CorsMiddleware::new()
+    let cors = CorsMiddleware::new()
         .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
         .allow_origin(Origin::from("*"))
         .allow_credentials(false);
 
     let mut app = tide::with_state(pool);
-    app.middleware(rules);
-    app.at("/").get(|req: Request<PgPool>| async move {
-        let pool = req.state();
-        let activities = fetch_activities(pool).await?;
-        Ok(format!("{:?}", activities))
-    });
+    app.middleware(cors);
+    app.at("/")
+        .get(|_| async { Ok(String::from("Server is up.")) });
+    app.at("/activities")
+        .get(|req: Request<State>| async move {
+            let activities = fetch_activities(req.state()).await?;
+            Ok(to_json_response(activities))
+        })
+        .post(|req: Request<State>| async move {
+            let activity = create_activity(req.state(), "Hai").await?;
+            Ok(to_json_response(&activity))
+        });
     app
+}
+
+fn to_json_response<T: Serialize + std::fmt::Debug>(entitiy: T) -> Response {
+    Response::new(StatusCode::Ok)
+        .body_json(&entitiy)
+        .expect(&format!("Failed to serialize {:?}", entitiy))
 }
--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,19 +1,29 @@
+use serde::Serialize;
 use sqlx::postgres::PgPool;
-use sqlx::Result as SqlxResult;
 
-#[derive(Debug, sqlx::FromRow)]
+#[derive(Debug, Serialize, sqlx::FromRow)]
 pub struct Activity {
     name: String,
     id: i32,
 }
 
-pub async fn fetch_activities(pool: &PgPool) -> SqlxResult<Vec<Activity>> {
+pub async fn fetch_activities(pool: &PgPool) -> sqlx::Result<Vec<Activity>> {
     sqlx::query_as!(Activity, "select * from activities")
         .fetch_all(pool)
         .await
 }
 
-pub async fn build_pool(db_url: &String, num_cons: u32) -> SqlxResult<PgPool> {
+pub async fn create_activity(pool: &PgPool, name: &str) -> sqlx::Result<Activity> {
+    sqlx::query_as!(
+        Activity,
+        "INSERT INTO activities ( name ) VALUES ( $1 ) RETURNING * ",
+        name
+    )
+    .fetch_one(pool)
+    .await
+}
+
+pub async fn build_pool(db_url: &String, num_cons: u32) -> sqlx::Result<PgPool> {
     PgPool::builder()
         .max_size(num_cons) // maximum number of connections in the pool
         .build(&db_url)
