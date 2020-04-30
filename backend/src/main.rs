99e00a02fca04954b28ca04f6a90fe898b419077
Dennis Collective <dennis.collective@gmail.com>
2020-04-30 03:07:34 -0700

    set log level from the RUST_LOG environment

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,6 @@
 use dotenv;
 use std::env;
-use tracing::{Level};
-use tracing_subscriber;
+use tracing_subscriber::EnvFilter;
 
 mod api;
 mod db;
@@ -9,11 +8,13 @@ mod db;
 #[async_std::main]
 async fn main() -> Result<(), anyhow::Error> {
     dotenv::dotenv().ok();
-    tracing_subscriber::fmt().with_max_level(Level::TRACE).init();
-
     let db_url = env::var("DATABASE_URL")?;
     let db_cons = 10;
     let listen = "0.0.0.0:8080";
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
 
     let pool = db::build_pool(&db_url, db_cons).await?;
     api::new(pool).listen(listen).await?;
