43cdff973a174cbcc12101a878b709cce16cb907
Dennis Collective <dennis.collective@gmail.com>
2020-05-03 15:31:18 -0700

    deserialize csv rows into records

--- backend/src/db/import.rs
+++ backend/src/db/import.rs
@@ -1,29 +1,79 @@
 use csv::Trim;
+use serde::Deserialize;
 use sqlx::PgPool;
 use std::io;
 
 use crate::db::find_or_create_activity;
+use chrono::{DateTime, Utc};
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Record {
+    #[serde(with = "maybe_date")]
+    date: Option<DateTime<Utc>>,
+    exercise: String,
+    reps: String,
+    sets: String,
+}
+
+struct Import<T> {
+    reader: csv::Reader<T>,
+}
+
+impl<T: io::Read> Import<T> {
+    fn from(spreadsheet: T) -> Self {
+        Self {
+            reader: csv::ReaderBuilder::new()
+                .delimiter(b'\t')
+                .trim(Trim::All)
+                .from_reader(spreadsheet),
+        }
+    }
+
+    pub fn rows(mut self) -> Vec<Record> {
+        self.reader
+            .deserialize::<Record>()
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(|r| Option::is_some(&r.date))
+            .collect()
+    }
+}
 
 #[allow(dead_code)]
 pub async fn import(pool: &PgPool, spreadsheet: String) -> anyhow::Result<()> {
-    let mut reader = reader(spreadsheet.as_bytes());
-
-    for record in reader.records() {
-        let r = record?;
-        let name = r.get(1).unwrap();
-        if !name.is_empty() {
-            find_or_create_activity(&pool, name).await?;
-        }
+    for row in Import::from(spreadsheet.as_bytes()).rows() {
+        find_or_create_activity(&pool, &row.exercise).await?;
     }
 
     Ok(())
 }
 
-fn reader<R: io::Read>(spreadsheet: R) -> csv::Reader<R> {
-    csv::ReaderBuilder::new()
-        .delimiter(b'\t')
-        .trim(Trim::All)
-        .from_reader(spreadsheet)
+mod maybe_date {
+    use chrono::{DateTime, TimeZone, Utc};
+    use serde::{self, Deserialize, Deserializer};
+
+    const FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+    const TWO_OCLOCK_PM: &str = " 14:00:00";
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<DateTime<Utc>>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let mut s = String::deserialize(deserializer)?;
+        let val = if s.is_empty() {
+            None
+        } else {
+            s.push_str(TWO_OCLOCK_PM);
+
+            Some(
+                Utc.datetime_from_str(&s, FORMAT)
+                    .map_err(serde::de::Error::custom)?,
+            )
+        };
+
+        Ok(val)
+    }
 }
 
 #[cfg(test)]
