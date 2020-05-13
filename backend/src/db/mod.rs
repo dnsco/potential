64b799c36beb993f0c70e6971af5ec2497da0cd5
Dennis Collective <dennis.collective@gmail.com>
2020-05-13 15:29:44 -0700

    add reset endpoint

--- backend/src/db/mod.rs
+++ backend/src/db/mod.rs
@@ -1,7 +1,7 @@
 use secrecy::{ExposeSecret, SecretString};
 use sqlx::postgres::PgPool;
 
-use crate::db::users::UsersRepo;
+use crate::db::users::{User, UsersRepo};
 pub use activities::{ActivitiesRepo, Activity, NewActivity};
 pub use activity_events::ActivityEventsRepo;
 pub use import::DbImport;
@@ -40,4 +40,16 @@ pub async fn build_pool(db_url: DbUrl, num_cons: u32) -> sqlx::Result<PgPool> {
         .max_size(num_cons) // maximum number of connections in the pool
         .build(db_url.expose_secret())
         .await
+}
+
+pub async fn reset(pool: &PgPool) -> sqlx::Result<User> {
+    sqlx::query!("TRUNCATE TABLE users CASCADE")
+        .execute(pool)
+        .await?;
+
+    sqlx::query!("ALTER SEQUENCE users_id_seq RESTART WITH 1")
+        .execute(pool)
+        .await?;
+
+    pool.users().create().await
 }
--- backend/src/web/mod.rs
+++ backend/src/web/mod.rs
@@ -2,12 +2,13 @@ use crate::db::{DbImport, RepoFactory};
 
 use http_types::headers::HeaderValue;
 use http_types::StatusCode;
+use sqlx::PgPool;
 use std::env;
 use tide::security::{CorsMiddleware, Origin};
 
 mod api;
 
-pub fn new<T: RepoFactory + Send + Sync + 'static>(pool: T) -> tide::Server<T> {
+pub fn new(pool: PgPool) -> tide::Server<PgPool> {
     let cors = CorsMiddleware::new()
         .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
         .allow_origin(Origin::from("*"))
@@ -15,14 +16,19 @@ pub fn new<T: RepoFactory + Send + Sync + 'static>(pool: T) -> tide::Server<T> {
 
     let mut app = tide::with_state(pool);
     app.middleware(cors);
+
     app.at("/")
         .get(|_| async { Ok(String::from("Server is up.")) });
-    app.at("/import").get(import);
+
     let mut api = app.at("/api");
     api.at("/activities")
         .get(api::activities::list)
         .post(api::activities::create);
     api.at("/activity_events").get(api::activity_events::list);
+
+    let mut db = app.at("/db");
+    db.at("/import").get(import);
+    db.at("/reset").get(reset);
 
     app
 }
@@ -30,7 +36,7 @@ pub fn new<T: RepoFactory + Send + Sync + 'static>(pool: T) -> tide::Server<T> {
 pub async fn import(req: tide::Request<impl RepoFactory>) -> tide::Result<String> {
     let strength_url = env::var("STRENGTH_URL")?;
     let pool = req.state();
-    let user = pool.users().create().await?;
+    let user = pool.users().find(1).await?;
 
     DbImport::from(pool, strength_url)?
         .run(&user)
@@ -38,4 +44,11 @@ pub async fn import(req: tide::Request<impl RepoFactory>) -> tide::Result<String
         .map_err(|e| tide::Error::from_str(StatusCode::InternalServerError, e))?;
 
     Ok(String::from("Import Success"))
+}
+
+pub async fn reset(req: tide::Request<PgPool>) -> tide::Result<String> {
+    let user = crate::db::reset(req.state())
+        .await
+        .map_err(|e| tide::Error::from_str(StatusCode::InternalServerError, e))?;
+    Ok(format!("db reset: {:?}", user))
 }
